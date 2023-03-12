a4cfc62f6fce0042961f28fedbac2b74ed1e5ebe
skywalkerpor79 <126904539+skywalkerpor79@users.noreply.github.com>
2023-03-12 20:37:50 +0100

    actualizacion 12/03/2023

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'blog',    
+    'tailwind',
+    'theme',
+]   
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
