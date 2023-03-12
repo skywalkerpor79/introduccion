f2d4705853fd89a49aad3fe09feeb35fd34f833e
skywalkerpor79 <126904539+skywalkerpor79@users.noreply.github.com>
2023-03-12 21:06:56 +0100

    hola senyores

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-#hola
+#hola senyores
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
